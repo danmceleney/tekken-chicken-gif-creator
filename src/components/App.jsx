75e34b84fa569dc57be9e24569fdf5555400a978
Nick DeJesus <ndejesus@sapient.com>
2017-07-05 18:59:28 -0400

    Getting started

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  handleSubmit() {
+    console.log('waddup');
+  }
+
+  render() {
+    return(
+      <div>
+        <form>
+          <input />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App;
