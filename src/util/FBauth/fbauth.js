b0ea12090451dfeb254416626245fda4e36d679c
Nick DeJesus <ndejesus@sapient.com>
2017-07-09 19:42:23 -0400

    derp

--- src/util/FBauth/fbauth.js
+++ src/util/FBauth/fbauth.js
@@ -4,7 +4,7 @@ export function fbauth(e) {
     const postData = {
         grant_type: 'convert_code',
         provider: 'facebook',
-        client_id: '2_vZN6ri',
+        client_id: config.client_id,
         client_secret: config.client_secret,
         auth_code: config.auth_code
     }
