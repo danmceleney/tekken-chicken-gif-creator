80e5b3c523e4a1c549ec578f6d4b3b5d1208dada
Nick DeJesus <ndejesus@sapient.com>
2017-07-25 12:44:05 -0400

    authentication token function. Needs to be incorporated with all API calls now

--- src/util/gyfcatMaker/gyfcatMaker.js
+++ src/util/gyfcatMaker/gyfcatMaker.js
@@ -1,3 +1,31 @@
+import config from '../FBauth/authConfig'
+
+function getToken(client_id, client_secret) {
+    let token_info = {}
+    const payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    };
+    fetch('https://api.gfycat.com/v1/oauth/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            payload
+        )
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        return token_info = {
+            token_type: json.token_type,
+            access_token: json.access_token
+        }
+    }).catch(err => {
+        console.log('something done goofed');
+    });
+}
 
 function statusCheck(name) {
     fetch(`https://api.gfycat.com/v1/gfycats/fetch/status/${name}`, {
@@ -30,5 +58,12 @@ function gifCutter(url, title, minutes, seconds, captions) {
         statusCheck(json.gfyname);
     })
 }
+
+function testFunction () {
+    return 'string';
+}
+
+const access = getToken(config.client_id, config.client_secret);
+console.log(getToken(config.client_id, config.client_secret));
 
 export default gifCutter;
