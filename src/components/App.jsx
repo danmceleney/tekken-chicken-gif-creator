db9a5732dc2f976cdaba49f85ed39fb87de523b6
Nick DeJesus <ndejesus@sapient.com>
2017-07-27 07:39:07 -0400

    refactoring and properly passing auth token

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,119 @@
 import React, { Component } from 'react';
-import gifCutter from '../util/gyfcatMaker/gyfcatMaker.js';
-import fetch from 'whatwg-fetch';
-import authConfig from '../util/FBauth/authConfig.js';
+import config from '../util/FBauth/authConfig';
 
-class App extends Component {
-  constructor(){
+const GyfcatAuthToken = App => class GyfcatAuthToken extends Component {
+  constructor() {
     super();
-
-    this.state = {}
+    this.state = {
+      token: null,
+      status: null
+    }
+  }
+  getToken(client_id, client_secret) {
+    //credentials to send with POST request to get auth token from API
+    const payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    };
+    //The actual API call to gyfcat. Passes in payload which is credentials. This is a Promise
+    fetch('https://api.gfycat.com/v1/oauth/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+        //initial API response that tells you the status of the POST request
+    }).then(response => {
+      return response.json();
+    }).then(json => {
+      this.setState({
+        token: json.access_token,
+        status: 'SUCCESS'
+      })
+    }).catch(error => {
+      this.token = null;
+      this.status = 'ERROR';
+    });
   }
 
-  handleSubmit(event) {
+  componentDidMount() {
+    if(!this.token) {
+      this.status = 'FETCHING';
+      this.getToken(config.client_id, config.client_secret);
+    }
+  }
+  render() {
+    console.log('HOC', this.state);
+    return <App token={this.state.token} status={this.state.status}/>;
+  }
+}
+
+class App extends Component {
+
+gifCutter(url, title, minutes, seconds, captions, auth) {
+    fetch('https://api.gfycat.com/v1/gfycats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${auth}`
+        },
+        body: JSON.stringify({
+            url,
+            title,
+            minutes,
+            seconds,
+            captions
+        })
+    }).then(response => {
+      console.log('fetch response', response);
+        return response.json();
+    }).then(json => {
+      console.log('whole json', json);
+        this.statusCheck(json.gfyname);
+    })
+}
+
+statusCheck(name) {
+    fetch(`https://api.gfycat.com/v1/gfycats/fetch/status/${name}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        console.log('STATUS', json);
+    })
+}
+
+fileDropper(secret) {
+  fetch('https://filedrop.gfycat.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({secret})
+  }).then(response => {
+    return response.json()
+  }).then(json => {
+    console.log(json);
+  })
+}
+
+  handleSubmit(event, token) {
     event.preventDefault();
-    const { url, title, minutes, seconds } = event.target;
-    gifCutter(url.value, title.value, minutes.value, seconds.value);
+    const { url, title, minutes, seconds, caption } = event.target;
+    const captionArray = [{text:caption.value}];
+    console.log(captionArray);
+    this.gifCutter(url.value, title.value, minutes.value, seconds.value, captionArray, token);
   }
 
   render() {
+    console.log('APP', this);
     return(
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={(e) => {this.handleSubmit(e, this.props.token)}}>
           <label>
             URL: <input type="text" name="url" />
           </label>
@@ -32,6 +126,9 @@ class App extends Component {
           <label>
             seconds: <input type="text" name="seconds" />
           </label>
+          <label>
+            caption: <input type="text" name="caption" />
+          </label>
           <input type="submit" value="Submit" />
         </form>
       </div>
@@ -39,4 +136,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default GyfcatAuthToken(App);
--- src/util/gyfcatMaker/gyfcatMaker.js
+++ src/util/gyfcatMaker/gyfcatMaker.js
@@ -1,38 +1,6 @@
 //importing config file for authentication, you can only get this if I send it to you
 import config from '../FBauth/authConfig'
 
-//function that retrieves auth token using fetch
-function getToken(client_id, client_secret) {
-    let token_info = {}
-    //credentials to send with POST request to get auth token from API
-    const payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret
-    };
-    //The actual API call to gyfcat. Passes in payload which is credentials. This is a Promise
-    fetch('https://api.gfycat.com/v1/oauth/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            payload
-        )
-        //initial API response that tells you the status of the POST request
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        //the actual data you get after the Promise is fulfilled.
-        return token_info = {
-            token_type: json.token_type,
-            access_token: json.access_token
-        }
-    }).catch(err => {
-        console.log('something done goofed');
-    });
-}
-
 //Check the status of the gyfcat that was created.
 function statusCheck(name) {
     fetch(`https://api.gfycat.com/v1/gfycats/fetch/status/${name}`, {
@@ -65,12 +33,5 @@ function gifCutter(url, title, minutes, seconds, captions) {
         statusCheck(json.gfyname);
     })
 }
-
-function testFunction () {
-    return 'string';
-}
-
-const access = getToken(config.client_id, config.client_secret);
-console.log(getToken(config.client_id, config.client_secret));
 
 export default gifCutter;
