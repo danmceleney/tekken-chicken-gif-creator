78c67717c5563d6e3786742f1b3a580cbdd6ed6c
Nick DeJesus <ndejesus@sapient.com>
2017-08-02 22:19:05 -0400

    styling stuff set up

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
